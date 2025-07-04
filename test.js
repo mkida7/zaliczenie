1a56da4d6688cd4d733f8c091e9a3227a54113de
mkida7 <mikolaj.kida7@gmail.com>
2025-07-04 17:20:13 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-const { add, subtract, multiply, divide } = require('./calculator');
+const a = 5;
+const b = 3;
 
-console.assert(add(2, 3) === 5, 'blad dodawanie');
-console.assert(subtract(5, 2) === 3, 'blad odejmowanie');
-console.assert(multiply(4, 2) === 8, 'blad mnozenie');
-console.assert(divide(10, 2) === 5, 'blad dzielenie');
+console.assert(add(a, b) === a + b, 'blad dodawanie'); 
+console.assert(subtract(a, b) === a - b, 'blad odejmowanie'); 
+console.assert(multiply(a, b) === a * b, 'blad mnozenie'); 
+console.assert(divide(a, b) === a / b, 'blad dzielenie'); 
 
 console.log('Wszystkie testy zdane');
